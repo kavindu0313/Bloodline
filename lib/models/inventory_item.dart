3b58d259e853b4585ffdcb40b2cb22eaceadf097
Shahen2002 <shahensenura125@gmail.com>
2025-04-29 17:02:00 +0530

    Update inventory_item.dart
    updated dart

--- lib/models/inventory_item.dart
+++ lib/models/inventory_item.dart
@@ -1,9 +1,10 @@
+//inventory item class
 class InventoryItem {
   final String bloodType;
   final int totalUnits;
   final DateTime oldestUnit;
   final DateTime latestExpiryDate;
-
+  //constructor
   InventoryItem({
     required this.bloodType,
     required this.totalUnits,
